9184750d078092bbce76878e22f6ce3b5cdb296b
lkbgift <lkbgift@gmail.com>
2013-05-11 23:24:22 -0400

    class ticket

--- header.php
+++ header.php
@@ -24,20 +24,19 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<!--:later - Make use of this for SEO-->
-			<!--Comment out h2-->
-			<!--<h2 class="site-description">--><?php // bloginfo( 'description' ); ?><!--</h2>-->
-		</div>
+	<header id="masthead" class="row site-header" role="banner">
+			<div class="site-branding">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<!--:later - Make use of this for SEO-->
+				<!--Comment out h2-->
+				<!--<h2 class="site-description">--><?php // bloginfo( 'description' ); ?><!--</h2>-->
+			</div>
+			<nav id="site-navigation" class="navigation-main" role="navigation">
+				<h1 class="menu-toggle"><?php _e( 'Menu', 'classticket' ); ?></h1>
+				<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'classticket' ); ?>"><?php _e( 'Skip to content', 'classticket' ); ?></a></div>
 
-		<nav id="site-navigation" class="navigation-main" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'classticket' ); ?></h1>
-			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'classticket' ); ?>"><?php _e( 'Skip to content', 'classticket' ); ?></a></div>
-
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="main" class="site-main">
