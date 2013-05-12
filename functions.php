396b314cf509df33b9c7fb1ac1e8bc6a947da737
lkbgift <lkbgift@gmail.com>
2013-05-11 22:54:18 -0400

    no errors in js

--- functions.php
+++ functions.php
@@ -128,11 +128,13 @@ add_action( 'widgets_init', 'classticket_widgets_init' );
 function classticket_scripts() {
 	wp_enqueue_style( 'ClassTicket-style', get_stylesheet_uri() );
 
+	wp_enqueue_script( 'ClassTicket-navigation', get_template_directory_uri() . '/js/vendor/custom.modernizr.js', array(), '20130506' );
+
 	wp_enqueue_script( 'ClassTicket-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'ClassTicket-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	wp_enqueue_script( 'Foundation-Sections', get_template_directory_uri() . '/js/foundation/foundation-ck.js', array( 'jquery' ), '20130511', true );
+	wp_enqueue_script( 'Foundation-Sections', get_template_directory_uri() . '/js/foundation.min.js', array( 'jquery' ), '20130511', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
